c38b3c5546a52811622601cc8195bca82bafd21d
Liliana Silva <lilianaods@outlook.com>
2023-07-31 21:38:19 -0400

    Add ContactName type alias

--- Chapter_2/src/demo.ts
+++ Chapter_2/src/demo.ts
@@ -1,24 +1,14 @@
-interface Contact extends Address {
-    id: number;
-    name: string;
-    birthDate?: Date; // This property is optional
-};
+// Type alias
+type ContactName = string; // If I wanted to change all ContactName references
 
-interface Address {
-    line1: string;
-    line2: string;
-    province: string;
-    region: string;
-    postalCode: string;
+interface Contact {
+    id: number;
+    name: ContactName;
+    birthDate?: Date; // This property is optional
 };
 
 let primaryContact: Contact = {
     birthDate: new Date('01-01-1990'),
     id: 12345,
     name: 'John Doe',
-    line1: '123',
-    line2: '456',
-    postalCode: '123-456',
-    province: 'Province',
-    region: 'Region',
 };
