817930ce98de00a69ba54a8e2bbc386b13a5d810
Liliana Silva <lilianaods@outlook.com>
2023-07-31 22:03:59 -0400

    Update clone funcion with generic types

--- Chapter_2/src/demo.ts
+++ Chapter_2/src/demo.ts
@@ -4,10 +4,22 @@ interface Contact {
     // clone(name: string): Contact;
 };
 
+interface UserContact<TExternalId> {
+    id: number;
+    name: string;
+    username: string;
+    externalId: TExternalId;
+    // loadExternalId(): Task<TExternalId>;
+};
+
 // function clone(source: Contact, func: (source: Contact) => Contact): Contact {
-function clone(source: Contact): Contact {
+function clone<T1, T2 extends T1>(source: T1): T2 {
     return Object.apply({}, source);
 };
 
 const a: Contact = { id: 123, name: 'Homer Simpsom' };
-const b = clone(a);
+const b = clone<Contact, UserContact<string>>(a);
+
+const dateRange = { startDate: Date.now(), endDate: Date.now() };
+const dateRangeCopy = clone(dateRange);
+
