1c7341e543f4298adeb285f8b48f8dcffdd5c42a
Liliana Silva <lilianaods@outlook.com>
2023-07-31 21:34:43 -0400

    Update Contact interface with Address interface

--- Chapter_2/src/demo.ts
+++ Chapter_2/src/demo.ts
@@ -1,11 +1,24 @@
-interface Contact {
+interface Contact extends Address {
     id: number;
     name: string;
     birthDate?: Date; // This property is optional
-}
+};
+
+interface Address {
+    line1: string;
+    line2: string;
+    province: string;
+    region: string;
+    postalCode: string;
+};
 
 let primaryContact: Contact = {
     birthDate: new Date('01-01-1990'),
     id: 12345,
     name: 'John Doe',
+    line1: '123',
+    line2: '456',
+    postalCode: '123-456',
+    province: 'Province',
+    region: 'Region',
 };
