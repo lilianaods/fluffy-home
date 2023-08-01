82556f4185bcccecb87d444f7f3d128b2715cb26
Liliana Silva <lilianaods@outlook.com>
2023-07-31 22:21:16 -0400

    Add challenge for types

---
+++ Chapter_2/challenge/app.ts
@@ -0,0 +1,41 @@
+enum TodoItemStatus {
+    Done = 'done',
+    InProgress = 'in-progress',
+    Todo = 'todo',
+};
+
+interface TodoItem {
+    id: number;
+    title: string;
+    status: TodoItemStatus;
+    completedOn?: Date;
+};
+
+const todoItems: TodoItem[] = [
+    { id: 1, title: "Learn HTML", status: TodoItemStatus.Done, completedOn: new Date("2021-09-11") },
+    { id: 2, title: "Learn TypeScript", status: TodoItemStatus.InProgress },
+    { id: 3, title: "Write the best app in the world", status: TodoItemStatus.Todo },
+]
+
+function addTodoItem(todo: string): TodoItem {
+    const id = getNextId(todoItems);
+
+    const newTodo = {
+        id,
+        title: todo,
+        status: TodoItemStatus.Todo,
+    };
+
+    todoItems.push(newTodo);
+
+    return newTodo;
+}
+
+// function getNextId<T extends TodoItem>(items: T[]): number {
+function getNextId<T extends { id: number }>(items: T[]): number {
+    return items.reduce((max, x) => x.id > max ? x.id : max, 0) + 1;
+};
+
+const newTodo = addTodoItem("Buy lots of stuff with all the money we make from the app")
+
+console.log(JSON.stringify(newTodo))
