a50158e8c1b3736a875ff1bcd28dcf8ab2f40d2f
Liliana Silva <lilianaods@outlook.com>
2023-08-01 22:09:39 -0400

    Add function with keyof operator

--- Chapter_3/src/demo.ts
+++ Chapter_3/src/demo.ts
@@ -1,15 +1,6 @@
 type ContactName = string;
 type ContactBirthDate = Date | number | string;
-
-interface Address {
-    line1: string;
-    line2: string;
-    province: string;
-    region: string;
-    postalCode: string;
-};
-
-type ContactStatus = 'active'| 'inactive'| 'new';
+type ContactStatus = 'active' | 'inactive' | 'new';
 
 interface Contact {
     id: number;
@@ -18,24 +9,19 @@ interface Contact {
     status?: ContactStatus;
 };
 
-// Alternatives
-// interface AdressableContact extends Contact, Address { };
-type AddressableContact = Contact & Address;
-
-function getBirthDate(contact: Contact) {
-    if (typeof contact.birthDate === 'number') {
-        return new Date(contact.birthDate);
-    }
-    else if (typeof contact.birthDate === 'string') {
-        return Date.parse(contact.birthDate);
-    }
-    else {
-        return contact.birthDate;
-    }
-};
-
 let primaryContact: Contact = {
     id: 12345,
     name: 'Jamie Johnson',
     status: 'new',
 };
+
+// All of the properties of the Contact type
+type ContactFields = keyof Contact;
+
+const field: ContactFields = 'status';
+
+function getValue<T, U extends keyof T>(source: T, propertyName: U) {
+    return source[propertyName];
+};
+
+const value = getValue({ min: 0, max: 200 }, 'max');
